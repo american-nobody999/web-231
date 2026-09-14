06e17d78382f248dc2f89d90b22c2405b0ae2cec
american-nobody999 <american_nobody999@proton.me>
2026-09-13 18:54:45 -0700

    Add js03_txt.js with week days and comments
    Initial commit of js03_txt.js with week days and comments.

---
+++ week-4/chapter/js03_txt.js
@@ -0,0 +1,17 @@
+/*    JavaScript 7th Edition
+     Chapter 3
+     Chapter case
+
+     Tipton Turbines
+     Program to display games results in a web table
+     Author: 
+     Date:   
+
+     Filename: js03.js
+ */
+
+     //days of the week
+let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+//We are going to take this and Ive got some fun ideas we can do with flash cards for the younger kids. 
+//This is going to be used in a painting games with colors, mixing the pallets, also gives matching, and creativity 
